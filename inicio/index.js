ac15073c75d375a6bfb1973abd67d5ec7bb3bf24
samu1959 <samuel.palaciosp@autonoma.edu.co>
2026-09-15 17:26:14 -0500

    module register , create module cuenta , modify index

---
+++ inicio/index.js
@@ -0,0 +1,41 @@
+
+
+function bienvenida() {
+
+    alert("¡Bienvenido a PulsePods! 🎧");
+
+}
+
+
+
+
+function mostrarOferta() {
+
+    alert(
+        "🔥 Oferta especial 🔥\n\n" +
+        "AirPods Pro 3\n" +
+        "$84.900\n\n" +
+        "¡Aprovecha nuestra promoción!"
+    );
+
+}
+
+
+
+let productos = 6;
+
+
+function mostrarProductos() {
+
+    document.getElementById("cantidadProductos").textContent =
+        productos + " productos disponibles";
+
+}
+
+
+function cambiarMensaje() {
+
+    document.getElementById("mensaje").textContent =
+        "Encuentra el accesorio perfecto para ti 🎧";
+
+}
